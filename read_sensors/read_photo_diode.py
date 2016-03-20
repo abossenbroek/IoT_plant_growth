9d40cec17f43ec0b0b789ace0a4c113272b4db83
Anton Bossenbroek <anton.bossenbroekatme.com>
2016-03-20 23:22:21 +0000

    Removed temperature code.

--- read_sensors/read_photo_diode.py
+++ read_sensors/read_photo_diode.py
@@ -22,29 +22,9 @@ def ConvertVolts(data,places):
   volts = round(volts,places)
   return volts
  
-# Function to calculate temperature from
-# TMP36 data, rounded to specified
-# number of decimal places.
-def ConvertTemp(data,places):
- 
-  # ADC Value
-  # (approx)  Temp  Volts
-  #    0      -50    0.00
-  #   78      -25    0.25
-  #  155        0    0.50
-  #  233       25    0.75
-  #  310       50    1.00
-  #  465      100    1.50
-  #  775      200    2.50
-  # 1023      280    3.30
- 
-  temp = ((data * 330)/float(1023))-50
-  temp = round(temp,places)
-  return temp
- 
+
 # Define sensor channels
 light_channel = 0
-temp_channel  = 1
  
 # Define delay between readings
 delay = 5
@@ -55,15 +35,10 @@ while True:
   light_level = ReadChannel(light_channel)
   light_volts = ConvertVolts(light_level,2)
  
-  # Read the temperature sensor data
-  temp_level = ReadChannel(temp_channel)
-  temp_volts = ConvertVolts(temp_level,2)
-  temp       = ConvertTemp(temp_level,2)
  
   # Print out results
   print "--------------------------------------------"
   print("Light: {} ({}V)".format(light_level,light_volts))
-  print("Temp : {} ({}V) {} deg C".format(temp_level,temp_volts,temp))
  
   # Wait before repeating loop
   time.sleep(delay)
