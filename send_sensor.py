c38c7d1f82df1bebfb452afd021520189610015a
Anton Bossenbroek <anton.bossenbroekatme.com>
2016-04-07 23:10:27 -0400

    Updated code such that ubidots is one of the options where data can be uploaded.

--- send_sensor.py
+++ send_sensor.py
@@ -28,6 +28,8 @@ config.readfp(open('send_sensor.cfg'))
 
 # Read general configuration options.
 update_delay = config.getfloat('general', 'update_delay')
+is_ubisoft_requested = config.getboolean('general', 'enable_ubisoft')
+is_thethings_requested = config.getboolean('general', 'enable_thethings')
 # Read hardware configuration options.
 spi_bus = config.getint('devices', 'spi_bus')
 mcp3008_spi_device = config.getint('devices', 'mcp3008_spi_device')
@@ -36,30 +38,38 @@ soil_channel = config.getint('devices', 'soil_channel')
 i2c_bus = config.getint('devices', 'i2c_bus')
 bme280_address = config.get('devices', 'bme280_address')
 # Read API configuration options.
-ubisoft_api_token = config.get('ubisoftApi', 'ubisoft_api_token')
-soil_level_api_key = config.get('ubisoftApi', 'soil_level_api_key')
-soil_volt_api_key = config.get('ubisoftApi', 'soil_volt_api_key')
-light_level_api_key = config.get('ubisoftApi', 'light_level_api_key')
-light_volt_api_key = config.get('ubisoftApi', 'light_volt_api_key')
-air_pressure_api_key = config.get('ubisoftApi', 'air_pressure_api_key')
-air_humidity_api_key = config.get('ubisoftApi', 'air_humidity_api_key')
-air_temperature_api_key = config.get('ubisoftApi', 'air_temperature_api_key')
+ubisoft_api_token = config.get('ubisoftAPI', 'ubisoft_api_token')
+soil_level_ubi_api_key = config.get('ubisoftAPI', 'soil_level_api_key')
+soil_volt_ubi_api_key = config.get('ubisoftAPI', 'soil_volt_api_key')
+light_level_ubi_api_key = config.get('ubisoftAPI', 'light_level_api_key')
+light_volt_ubi_api_key = config.get('ubisoftAPI', 'light_volt_api_key')
+air_pressure_ubi_api_key = config.get('ubisoftAPI', 'air_pressure_api_key')
+air_humidity_ubi_api_key = config.get('ubisoftAPI', 'air_humidity_api_key')
+air_temperature_ubi_api_key = config.get('ubisoftAPI', 'air_temperature_api_key')
 
 # Open SPI bus
 spi = spidev.SpiDev()
 spi.open(spi_bus, mcp3008_spi_device)
 
-# Create an ApiClient object
-api = ApiClient(token = ubisoft_api_token)
 
-# Get a Ubidots Variable
-soil_sensor_level = api.get_variable(soil_level_api_key)
-soil_sensor_volt = api.get_variable(soil_volt_api_key)
-light_sensor_level = api.get_variable(light_level_api_key)
-light_sensor_volt = api.get_variable(light_volt_api_key)
-air_pressure_sensor = api.get_variable(air_pressure_api_key)
-air_humidity_sensor = api.get_variable(air_humidity_api_key)
-air_temperature_sensor = api.get_variable(air_temperature_api_key)
+if is_ubisoft_requested:
+    # Create an ApiClient object
+    ubi_api = ApiClient(token = ubisoft_api_token)
+
+    # Get a Ubidots Variable
+    soil_sensor_level_ubi = ubi_api.get_variable(soil_level_ubi_api_key)
+    soil_sensor_volt_ubi = ubi_api.get_variable(soil_volt_ubi_api_key)
+    light_sensor_level_ubi = ubi_api.get_variable(light_level_ubi_api_key)
+    light_sensor_volt_ubi = ubi_api.get_variable(light_volt_ubi_api_key)
+    air_pressure_sensor_ubi = ubi_api.get_variable(air_pressure_ubi_api_key)
+    air_humidity_sensor_ubi = ubi_api.get_variable(air_humidity_ubi_api_key)
+    air_temperature_sensor_ubi = ubi_api.get_variable(air_temperature_ubi_api_key)
+
+
+if is_thethings_requested:
+    raise Exception("thethings is not yet implemented.")
+
+
 
 # Set up communication to BME280.
 bme280_i2c.set_default_bus(i2c_bus)
@@ -68,40 +78,47 @@ bme280.setup()
 
 while True:
 
-    channel_level = ReadChannel(soil_channel)
-    channel_volts = ConvertVolts(channel_level, 4)
+    soil_channel_level = ReadChannel(soil_channel)
+    soil_channel_volts = ConvertVolts(soil_channel_level, 4)
 
-    response = soil_sensor_level.save_value({"value": channel_level})
-    print("Soil level meter writing: ")
-    print(response)
-    response = soil_sensor_volt.save_value({"value": channel_volts})
-    print("Soil volt meter writing: ")
-    print(response)
-
-    channel_level = ReadChannel(light_channel)
-    channel_volts = ConvertVolts(channel_level, 4)
-
-    response = light_sensor_level.save_value({"value": channel_level})
-    print("Light level meter writing: ")
-    print(response)
-    response = light_sensor_volt.save_value({"value": channel_volts})
-    print("Light volt meter writing: ")
-    print(response)
+    light_channel_level = ReadChannel(light_channel)
+    light_channel_volts = ConvertVolts(light_channel_level, 4)
 
     all_bme280_data = bme280.read_all()
 
-    response = air_pressure_sensor.save_value({"value":
-                                               all_bme280_data.pressure})
-    print("Air pressure writing: ")
-    print(response)
-    response = air_humidity_sensor.save_value({"value":
-                                               all_bme280_data.humidity})
-    print("Air humidty writing: ")
-    print(response)
-    response = air_temperature_sensor.save_value({"value":
-                                                  all_bme280_data.temperature})
-    print("Air temperature writing: ")
-    print(response)
+
+    if is_ubisoft_requested:
+        print('------------------------')
+        response = light_sensor_level_ubi.save_value({"value": soil_channel_level})
+        print('light sensor level')
+        print(response)
+        response = light_sensor_volt_ubi.save_value({"value": soil_channel_volts})
+        print('light sensor volt')
+        print(response)
+        response = soil_sensor_level_ubi.save_value({"value": light_channel_level})
+        print('soil sensor level')
+        print(response)
+        response = soil_sensor_volt_ubi.save_value({"value": light_channel_volts})
+        print('soil sensor volt')
+        print(response)
+
+        response = air_pressure_sensor_ubi.save_value({"value":
+                                                   all_bme280_data.pressure})
+        print('air pressure')
+        print(response)
+        response = air_humidity_sensor_ubi.save_value({"value":
+                                                   all_bme280_data.humidity})
+        print('air humidity')
+        print(response)
+        response = air_temperature_sensor_ubi.save_value({"value":
+                                                      all_bme280_data.temperature})
+        print('air temperature')
+        print(response)
+
+
+    if is_thethings_requested:
+
+
 
     # Wait before repeating loop
     time.sleep(update_delay)
